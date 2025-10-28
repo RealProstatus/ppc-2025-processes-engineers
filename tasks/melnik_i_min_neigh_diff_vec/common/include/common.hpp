36813b3ba96341c6f0e8f40da695afa6f6ccb733
Ivan Melnik <mel.sirvania@gmail.com>
2025-10-28 22:41:47 +0300

    Headers added

---
+++ tasks/melnik_i_min_neigh_diff_vec/common/include/common.hpp
@@ -0,0 +1,15 @@
+#pragma once
+
+#include <string>
+#include <tuple>
+
+#include "task/include/task.hpp"
+
+namespace melnik_i_min_neigh_diff_vec {
+
+using InType = std::vector<double>;
+using OutType = std::tuple<int, int>;
+using TestType = std::tuple<int, std::string>; //TODO rework in future
+using BaseTask = ppc::task::Task<InType, OutType>;
+
+}  // namespace melnik_i_min_neigh_diff_vec
