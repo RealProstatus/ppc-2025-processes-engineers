ff57ac29e626e30c2e4c8c98d430bf3fed0fc178
Ivan Melnik <mel.sirvania@gmail.com>
2025-10-29 23:35:57 +0300

    Better ValidationImpl

--- tasks/melnik_i_min_neigh_diff_vec/mpi/src/ops_mpi.cpp
+++ tasks/melnik_i_min_neigh_diff_vec/mpi/src/ops_mpi.cpp
@@ -17,19 +17,23 @@ MelnikIMinNeighDiffVecMPI::MelnikIMinNeighDiffVecMPI(const InType &in) {
 }
 
 bool MelnikIMinNeighDiffVecMPI::ValidationImpl() {
-  return !GetInput().empty() && GetInput().size() > 1;
-}
-
-bool MelnikIMinNeighDiffVecMPI::PreProcessingImpl() {
-  // move this into ValidationImpl
-  for (int i = 0; i < GetInput().size(); i++)
+    if (GetInput().size() < 2)
   {
-    if (!std::isfinite(GetInput()[i]))
+    return false;
+  }
+
+  for (auto it = GetInput().begin(); it != GetInput().end(); ++it)
+  {
+    if (!std::isfinite(*it))
     {
       return false;
     }
   }
-  
+
+  return true;
+}
+
+bool MelnikIMinNeighDiffVecMPI::PreProcessingImpl() {
   return true;
 }
 
