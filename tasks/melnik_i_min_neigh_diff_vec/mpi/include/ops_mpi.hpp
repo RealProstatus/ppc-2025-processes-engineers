1888285c4674c19fdf72f467ea9b5a1523dcaf87
Ivan Melnik <mel.sirvania@gmail.com>
2025-11-16 14:17:15 +0000

    Deleted extra function definitions

--- tasks/melnik_i_min_neigh_diff_vec/mpi/include/ops_mpi.hpp
+++ tasks/melnik_i_min_neigh_diff_vec/mpi/include/ops_mpi.hpp
@@ -26,19 +26,12 @@ class MelnikIMinNeighDiffVecMPI : public BaseTask {
     int index = -1;
   };
 
-  void GetMpiRankAndSize(int &rank, int &comm_size) const;
-  bool BroadcastGlobalSize(int &global_size, int rank);
-  void PrepareCountsAndDispls(std::vector<int> &counts, std::vector<int> &displs, int global_size, int comm_size,
-                              int rank) const;
-  void ScatterLocalSize(int &local_size, const std::vector<int> &counts) const;
   void ScatterData(std::vector<int> &local_data, const std::vector<int> &counts, const std::vector<int> &displs,
                    int rank);
-  [[nodiscard]] int ComputeLocalDispl(int local_size) const;
   void ComputeLocalMin(Result &local_res, const std::vector<int> &local_data, int local_size, int local_displ) const;
   void HandleBoundaryDiffs(Result &local_res, int local_size, const std::vector<int> &local_data, int local_displ,
                            int rank, int comm_size) const;
   void ReduceAndBroadcastResult(Result &global_res, const Result &local_res) const;
-  bool SetOutputFromGlobalResult(const Result &global_res);
 };
 
 }  // namespace melnik_i_min_neigh_diff_vec
